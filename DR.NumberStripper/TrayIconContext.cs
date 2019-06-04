ec0f1d92ff34cbe2e402c6a0a83d59b07c947007
Bo Bendtsen <bben@dr.dk>
2019-06-04 14:55:25 +0200

    Add project files.

---
+++ DR.NumberStripper/TrayIconContext.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using WK.Libraries.SharpClipboardNS;
+
+namespace DR.NumberStripper
+{
+    internal class TrayIconContext : ApplicationContext
+    {
+        private readonly NotifyIcon _trayIcon;
+        private Assembly _assembly => typeof(TrayIconContext).GetTypeInfo().Assembly;
+        
+        private List<string> _history = new List<string>();
+
+        private const int MaxItems = 8;
+        private MenuItem _startCurrent;
+        private MenuItem[] _baseItems;
+        private SharpClipboard _clipboard;
+        private readonly KeyboardHook _startHook;
+
+        public TrayIconContext(string[] args)
+        {
+            _startCurrent = new MenuItem("Try to &Start current (Ctrl+Alt+Shift+S)", StartCurrent);
+            if (!Clipboard.ContainsText() || !IsStartable(Clipboard.GetText()))
+            {
+                _startCurrent.Enabled = false;
+            }
+            _baseItems = new[]
+            {
+                new MenuItem("-"),
+                _startCurrent,
+                new MenuItem("&About", About),
+                new MenuItem("E&xit", Exit),
+            };
+
+            _trayIcon = new NotifyIcon()
+            {
+                Icon = new System.Drawing.Icon(_assembly.GetManifestResourceStream("DR.NumberStripper.Icon.ico") ?? throw new InvalidOperationException()),
+                ContextMenu = new ContextMenu(_baseItems),
+                Visible = true,
+                Text = "NumberStripper",
+            };
+            
+            _clipboard = new SharpClipboard();
+            _clipboard.ClipboardChanged += ClipboardChanged;
+            _startHook = new KeyboardHook();
+            _startHook.RegisterHotKey(ModifierKeys.Control | ModifierKeys.Alt | ModifierKeys.Shift, Keys.S);
+            _startHook.KeyPressed += StartCurrent;
+        }
+
+        private void UpdateHistory(string text)
+        {
+            lock (_history)
+            {
+                if (_history.Contains(text))
+                    return;
+
+                _history.Add(text);
+
+                while (_history.Count > MaxItems)
+                {
+                    _history.RemoveAt(0);
+                }
+
+                _trayIcon.ContextMenu = new ContextMenu(
+                    _history.Select(x => IsStartable(x) ? 
+                        new MenuItem(x, Start) : new MenuItem(x, Copy)).Reverse().Concat(_baseItems).ToArray());
+            }
+        }
+
+        private bool IsStartable(string text)
+        {
+            if (File.Exists(text))
+            {
+                return true;
+            }
+
+            if (Directory.Exists(text))
+            {
+                return true;
+            }
+
+            if (Uri.IsWellFormedUriString(text, UriKind.Absolute))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            _trayIcon.Visible = false;
+            _clipboard?.Dispose();
+            Application.Exit();
+        }
+
+        void About(object sender, EventArgs e)
+        {
+            var aboutText = string.Empty;
+            var aboutCaption = string.Empty;
+
+            if (Attribute.IsDefined(_assembly, typeof(AssemblyDescriptionAttribute)))
+            {
+                var a = (AssemblyDescriptionAttribute)Attribute.GetCustomAttribute(_assembly, typeof(AssemblyDescriptionAttribute));
+                aboutText = a.Description;
+            }
+
+            if (Attribute.IsDefined(_assembly, typeof(AssemblyTitleAttribute)))
+            {
+                var a = (AssemblyTitleAttribute)Attribute.GetCustomAttribute(_assembly, typeof(AssemblyTitleAttribute));
+                aboutCaption = a.Title;
+            }
+
+            MessageBox.Show(aboutText, aboutCaption, MessageBoxButtons.OK);
+        }
+
+        void Start(object sender, EventArgs e)
+        {
+            if (sender is MenuItem menuItem)
+            {
+                var process = System.Diagnostics.Process.Start(menuItem.Text);
+            }
+        }
+
+        void StartCurrent(object sender, EventArgs e)
+        {
+            if (!Clipboard.ContainsText()) return;
+
+            var text = Clipboard.GetText();
+
+            if (IsStartable(text))
+            {
+                var process = System.Diagnostics.Process.Start(text);
+            }
+        }
+
+
+        void Copy(object sender, EventArgs e)
+        {
+            if (sender is MenuItem menuItem)
+            {
+                Clipboard.SetText(menuItem.Text);
+            }
+        }
+
+        private Regex EscapedUncPath = new Regex(@"^\\{4}", RegexOptions.Compiled);
+        private Regex WhatsOnPrdNmr = new Regex(@"^[01]-\d{3}-\d{2}-\d{4}-\d$", RegexOptions.Compiled);
+        
+        private void ClipboardChanged(Object sender, SharpClipboard.ClipboardChangedEventArgs e)
+        {
+            // Is the content copied of text type?
+            if (e.ContentType == SharpClipboard.ContentTypes.Text)
+            {
+                var text = _clipboard.ClipboardText;
+                if (WhatsOnPrdNmr.IsMatch(text))
+                {
+                    Clipboard.SetText(text.Replace("-",String.Empty));
+                }
+                else if (EscapedUncPath.IsMatch(text))
+                {
+                    Clipboard.SetText(text.Replace(@"\\",@"\"));
+                }
+
+                text = Clipboard.GetText();
+                UpdateHistory(text);
+                _startCurrent.Enabled = IsStartable(text);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+
+# DR.NummerStripper
+Lille værktøj til at rense productionsnumre kopieret fra WhatsOn. Og til at un-escape UNC-stier. 
+
+## Brug
+Tilføj som start-program. Tryk Ctrl+Alt+Shift+S for at prøve at starte indholdet af udklipningsholderen.
